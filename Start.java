cdc55d6012b1ba8394e435d9e2543aa2411a56a6
tisga455 <tohominatisha2004@gmail.com>
2025-02-04 02:09:48 +0600

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,19 @@
+import book.Form3;
+import book.Form4;
+import book.Form5;
+import book.Form7;
+import book.Form8;
+import book.Form9;
+import book.Form10;
+import book.Form11;
+import book.Form12;
+import book.Form13;
+public class Start 
+{
+    public static void main(String[] args)
+    {
+        Form3 f3 = new Form3();
+        f3.setVisible(true);
+    }
+}
+
